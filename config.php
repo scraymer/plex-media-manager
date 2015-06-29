e7113bc27eb65e8a49d2afcb3ca9f9618216c786
Samuel Craymer <scraymer@users.noreply.github.com>
2015-06-29 15:40:18 -0400

    Create config.php
    added configuration variables which will be used in get_media.php and add_media.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+// The directory to look in for new media files
+$config['media_dir'] = "/Users/sam/Downloads/uTorrent/complete";
+
+// The file extensions to look for (comma seperated)
+$config['media_extensions'] = "avi,mkv,mp4,mov";
+
+// The location our your plex media server movie directory
+$config['media_dir_movies'] = "/Volumes/Sam-PowerMac/media/movies";
+
+// The location of your plex media server series (tv-shows) directory
+$config['media_dir_series'] = "/Volumes/Sam-PowerMac/media/tv-shows";
+
+?>
